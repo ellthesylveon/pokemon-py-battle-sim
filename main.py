2dfeeacad6967525419fba9721784650fc41a509
sarracenia <99730925+sarra-fell@users.noreply.github.com>
2024-12-06 19:45:02 -0700

    added rudimentary type effectiveness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math as mt
 import random
+import copy
 
 import statdex
 from movedex import *
@@ -29,6 +30,23 @@ class Pokemon:
             print(f'{self.name} fainted!')
             return False
 
+    # Returns a dictionary with a type chart that reflects the current state of the pokemon
+    def create_type_chart(self):
+        # Start with the chart of the its first type
+        type_chart = copy.deepcopy(expanded_type_chart[self.type_[0]])
+
+        if len(self.type_) > 1:
+            # It isnt a copy so this is not to be modified
+            second_type_chart = expanded_type_chart[self.type_[1]]
+
+            # Combine with the type chart of the 2nd type
+            for t in type_list:
+                type_chart.t *= second_type_chart.t
+
+        # Here abilities should be factored into the type chart but we can implement that later
+
+        return type_chart
+
     def calculate_stats(self) -> None:
         temp_base = list(self.base_stats.values())
         hp_stat: int = mt.floor(
@@ -53,7 +71,27 @@ class Pokemon:
         if move.accuracy < 100 & random.randint(0, 100) > move.accuracy:
             print(f'{self.name}\'s attack missed!')
         else:
-            damage = damage_calc(rel_atk, rel_def, self.level, move.power)
+            damage_modifier: float = 1
+
+            # Apply STAB modifier
+            if move.type_ in self.type_:
+                damage_modifier*=1.5
+
+            # Apply Strength/Resistance modifier
+            target_type_chart = target.create_type_chart()
+            type_effectiveness = target_type_chart[move.type_]
+
+            if type_effectiveness > 1:
+                print("It's super effective!")
+            elif type_effectiveness == 0:
+                print(f'The enemy {target.name} is unaffected...')
+                return
+            elif type_effectiveness < 1:
+                print("It's not very effective...")
+
+            damage_modifier*=type_effectiveness
+
+            damage = damage_calc(rel_atk, rel_def, self.level, move.power, damage_modifier)
             if damage > target.hp: damage = target.hp
             target.hp -= damage
             print(f'The enemy {target.name} lost {round(damage / target.stats[0] * 100)}% of its HP!')
@@ -85,9 +123,11 @@ def battle(player: Pokemon, opponent: Pokemon) -> None:
         player.attack(move, opponent)
 
 
-def damage_calc(rel_atk: int, rel_def: int, level: int, power: int | float) -> int:
+def damage_calc(rel_atk: int, rel_def: int, level: int, power: int | float, damage_modifier: float) -> int:
     damage: int = round(
         ((((((2 * level) / 5) + 2) * power * (rel_atk / rel_def)) / 50) + 2) * (random.randint(85, 100) / 100))
+
+    damage*=damage_modifier
     return damage
 
 
@@ -117,7 +157,7 @@ def move_select(player: Pokemon) -> Move:
 def main() -> None:
     player: Pokemon = Pokemon(statdex.mew['name'], 100, statdex.mew['base stats'], statdex.mew['type'],
                               statdex.mew['ability'], statdex.mew['evs'], statdex.mew['ivs'], 'Hardy', None,
-                              [], [PSYCHIC, MEGA_PUNCH, HYPER_VOICE, BOOMBURST])
+                              [], [PSYCHIC, DARK_PULSE, HYPER_VOICE, BOOMBURST])
     opponent: Pokemon = player
     battle(player, opponent)
 
